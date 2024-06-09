7ae92caff282548e1b6ba34748b177f872ff284b
LsmSystem <luissantander2002@gmail.com>
2024-06-08 21:09:36 -0400

    models

--- models/datapapelera.js
+++ models/datapapelera.js
@@ -5,15 +5,26 @@ const Datapapelera = {};
 Datapapelera.getAll = () => {
 
     const sql = `
-        SELECT
-            id,
-            name,
-            description,
-            image
-        FROM
-            categories
-        ORDER BY
-            name
+SELECT
+    CAST(id AS CHAR) AS id,
+    CAST(Codigo AS CHAR) AS Codigo,
+    CAST(OT AS CHAR) AS OT,
+    CAST(OTS AS CHAR) AS OTS,
+    Cliente,
+    Producto,
+    Fecha_Carguio,
+    Fecha_de_entrega,
+    CAST(Cantidad_Requerida AS CHAR) AS Cantidad_Requerida,
+    Fecha_Produccion,
+    CAST(Colores AS CHAR) AS Colores,
+    CAST(Largo_Interno AS CHAR) AS Largo_Interno,
+    CAST(Ancho_Interno AS CHAR) AS Ancho_Interno,
+    CAST(Alto_Interno AS CHAR) AS Alto_Interno,
+    Calidad,
+    Tipo_de_Caja
+FROM
+    pedidos
+
     `;
 
     return db.manyOrNone(sql);
