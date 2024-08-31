043d106a5cb25bd1e8f1497167b657532d87838a
systemlsm <systemlsm61@gmail.com>
2024-08-30 20:11:11 -0400

    findByStatus

--- routes/ordersRoutes.js
+++ routes/ordersRoutes.js
@@ -2,6 +2,7 @@ const OrdersController = require('../controllers/ordersController');
 const passport = require('passport');
 
 module.exports = (app) => {
-   app.get('/api/orders/findByStatus/:status', passport.authenticate('jwt', {session: false}), OrdersController.findByStatus);
+
+   app.get('/api/orders/findByStatus/:status', OrdersController.findByStatus);
    
 }
