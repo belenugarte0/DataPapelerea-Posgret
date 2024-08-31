8bf2b4c4916071b4a3e9446be9d353c5cc3d301f
LsmSystem <luissantander2002@gmail.com>
2024-08-31 13:56:48 -0400

    updatePlanning

--- routes/datapapeleraRoutes.js
+++ routes/datapapeleraRoutes.js
@@ -3,10 +3,6 @@ const DatapapeleraController = require("../controllers/datapapeleraController");
 module.exports = (app) => {
   // TRAER DATOS
   app.get("/api/datapapelera/getAll/:status", DatapapeleraController.getAll);
-  app.get(
-    "/api/datapapelera/findByStatus/:status",
-    DatapapeleraController.findByStatus
-  );
 
   app.get(
     "/api/datapapelera/getPedidosRecientes/:zonas",
@@ -15,16 +11,10 @@ module.exports = (app) => {
 
   app.get("/api/datapapelera/getZonas", DatapapeleraController.getZonas);
 
-  app.get(
-    "/api/datapapelera/validateCodigo/:codigo",
-    DatapapeleraController.validateCodigo
-  );
-
   app.put(
     "/api/datapapelera/updateToStatus/:id/:status",
     DatapapeleraController.updateToStatus
   );
-
   app.put(
     "/api/datapapelera/updatePlanning",
     DatapapeleraController.updatePlanning
